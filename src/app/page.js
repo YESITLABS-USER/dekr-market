254e6caf92d66d7a713195a055c21ce5f69ee487
unknown <yesitlabs.karankumar@gmail.com>
2025-07-22 19:31:03 +0530

    push

--- src/app/page.js
+++ src/app/page.js
@@ -4,12 +4,12 @@ import Community from "@/components/Community";
 import HowDekrWorks from "@/components/HowDekrWorks";
 import JoinWaitList from "@/components/JoinWaitList";
 import Image from "next/image";
-import circleTexture from "../assets/circle-tecture.png";
-import OvalImages from "../assets/oval-texture.png";
-import homeScreen from "../assets/home-screen.png";
-import loadScreen from "../assets/load-screen.png";
-import discoverScreen from "../assets/discover-screen.png";
-import coachScreen from "../assets/coach-screen.png";
+// import circleTexture from "@/assets/circle-tecture.png";
+// import OvalImages from "@/assets/oval-texture.png";
+// import homeScreen from "@/assets/home-screen.png";
+// import loadScreen from "@/assets/load-screen.png";
+// import discoverScreen from "@/assets/discover-screen.png";
+// import coachScreen from "@/assets/coach-screen.png";
 import Link from "next/link";
 
 export default function Home() {
@@ -17,7 +17,8 @@ export default function Home() {
     <>
       <div className="hero-sec has-texture">
         <div className="texture circle-texture">
-          <Image src={circleTexture} alt="Texture Image" />
+          {/* <Image src={circleTexture} alt="Texture Image" /> */}
+          <Image src="/images/circle-tecture.png" alt="Circle texture" width={500} height={300} />
         </div>
         <div className="container">
           <div className="hero-inr row">
@@ -27,16 +28,20 @@ export default function Home() {
             <div className="hero-body">
               <div className="hero-images">
                 <div className="hero-img-single">
-                  <Image src={homeScreen} alt="Mobile Screen Preview" />
+                  {/* <Image src={homeScreen} alt="Mobile Screen Preview" /> */}
+                  <Image src="/images/home-screen.png" alt="home-screen" width={500} height={300} />
                 </div>
                 <div className="hero-img-single">
-                  <Image src={loadScreen} alt="Mobile Screen Preview" />
+                  {/* <Image src={loadScreen} alt="Mobile Screen Preview" /> */}
+                  <Image src="/images/load-screen.png" alt="load-screen" width={500} height={300} />
                 </div>
                 <div className="hero-img-single">
-                  <Image src={discoverScreen} alt="Mobile Screen Preview" />
+                  {/* <Image src={discoverScreen} alt="Mobile Screen Preview" /> */}
+                  <Image src="/images/discover-screen.png" alt="discover-screen" width={500} height={300} />
                 </div>
                 <div className="hero-img-single">
-                  <Image src={coachScreen} alt="Mobile Screen Preview" />
+                  {/* <Image src={coachScreen} alt="Mobile Screen Preview" /> */}
+                  <Image src="/images/coach-screen.png" alt="coach-screen" width={500} height={300} />
                 </div>
               </div>
 
@@ -74,7 +79,8 @@ export default function Home() {
 
       <div className="goals-sec has-texture">
         <div className="texture oval-texture">
-          <Image src={OvalImages} alt="Texture Oval" />
+          {/* <Image src={OvalImages} alt="Texture Oval" /> */}
+          <Image src="/images/circle-tecture.png" alt="circle-tecture" width={500} height={300} />
         </div>
         <div className="container">
           <div className="goals-inr">
@@ -174,3 +180,180 @@ export default function Home() {
     </>
   );
 }
+
+// "use client";
+
+// import Community from "@/components/Community";
+// import HowDekrWorks from "@/components/HowDekrWorks";
+// import JoinWaitList from "@/components/JoinWaitList";
+// import Image from "next/image";
+// import circleTexture from "../assets/circle-tecture.png";
+// import OvalImages from "../assets/oval-texture.png";
+// import homeScreen from "../assets/home-screen.png";
+// import loadScreen from "../assets/load-screen.png";
+// import discoverScreen from "../assets/discover-screen.png";
+// import coachScreen from "../assets/coach-screen.png";
+// import Link from "next/link";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <div className="hero-sec has-texture">
+//         <div className="texture circle-texture">
+//           <Image src={circleTexture} alt="Texture Image" />
+//         </div>
+//         <div className="container">
+//           <div className="hero-inr row">
+//             <div className="hero-heading">
+//               <h1>Stack the Future in Your Favor with Dekr.</h1>
+//             </div>
+//             <div className="hero-body">
+//               <div className="hero-images">
+//                 <div className="hero-img-single">
+//                   <Image src={homeScreen} alt="Mobile Screen Preview" />
+//                 </div>
+//                 <div className="hero-img-single">
+//                   <Image src={loadScreen} alt="Mobile Screen Preview" />
+//                 </div>
+//                 <div className="hero-img-single">
+//                   <Image src={discoverScreen} alt="Mobile Screen Preview" />
+//                 </div>
+//                 <div className="hero-img-single">
+//                   <Image src={coachScreen} alt="Mobile Screen Preview" />
+//                 </div>
+//               </div>
+
+//               <div className="hero-content">
+//                 <p>
+//                   Dekr combines real-time market signals with interactive
+//                   financial education and an AI chat coach to help you invest
+//                   confidently—even if you&#39;re just starting out.
+//                 </p>
+//                 <Link
+//                   href="#url"
+//                   className="cmn-btn"
+//                   data-bs-target="#joinModal"
+//                   data-bs-toggle="modal"
+//                 >
+//                   Join the Waitlist
+//                 </Link>
+//                 <p className="hero-content-btm-txt -d-flex">
+//                   or
+//                   <Link
+//                     href="https://discord.com/invite/wQdWAVNzBN"
+//                     target="_blank"
+//                   >
+//                     Join our Discord
+//                   </Link>
+//                   to help shape the future of investing together.
+//                 </p>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* <!-- Goals Section Begin --> */}
+
+//       <div className="goals-sec has-texture">
+//         <div className="texture oval-texture">
+//           <Image src={OvalImages} alt="Texture Oval" />
+//         </div>
+//         <div className="container">
+//           <div className="goals-inr">
+//             <div className="row">
+//               <div className="col-lg-6">
+//                 <div className="gaols-left">
+//                   <div className="sec-head mb-0">
+//                     <h2>Hit Your Real-Life Goals</h2>
+//                     <p>If your money’s just sitting, it’s not growing.</p>
+//                   </div>
+//                 </div>
+//               </div>
+//               <div className="col-lg-6">
+//                 <div className="goals-right">
+//                   <h3>
+//                     Smart investing turns “maybe someday” into “definitely
+//                     happening”:
+//                   </h3>
+//                   <div className="goals-list">
+//                     <ul>
+//                       <li>Save for a down payment</li>
+//                       <li>Take that dream vacation</li>
+//                       <li>Build a nest egg that actually matters</li>
+//                       <li>Fund your kid’s college dreams</li>
+//                       <li>Retire the way you want</li>
+//                     </ul>
+//                   </div>
+//                   <p>
+//                     With Dekr, you’re not just throwing darts—you’re taking
+//                     shots with a coach who knows where the target is.
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* <!-- End Of Goals Section --> */}
+
+//       <HowDekrWorks />
+
+//       {/* <!-- Performance Section Begin --> */}
+
+//       <div className="performance-sec cmn-100">
+//         <div className="container">
+//           <div className="performance-inr">
+//             <div className="sec-head head-warm-pink text-center mb-5">
+//               <h2 className="h3-title">Momentum Model Performance</h2>
+//               <p>
+//                 Smart investing isn&#39;t luck—it&#39;s strategy. Here&#39;s the
+//                 proof:
+//               </p>
+//             </div>
+//             <div className="performance-table">
+//               <table>
+//                 <thead>
+//                   <tr>
+//                     <th>Strategy</th>
+//                     <th>5-Year Return</th>
+//                     <th>1-Year Return</th>
+//                   </tr>
+//                 </thead>
+//                 <tbody>
+//                   <tr>
+//                     <td>
+//                       <p>NASDAQ-100</p> <p>Buy and Hold (QQQ)</p>
+//                     </td>
+//                     <td>+154%</td>
+//                     <td>+25%</td>
+//                   </tr>
+//                   <tr>
+//                     <td>Dekr Momentum Model</td>
+//                     <td>+200%</td>
+//                     <td>+35%</td>
+//                   </tr>
+//                 </tbody>
+//               </table>
+//             </div>
+//             <div className="performance-sec-notes">
+//               <p>
+//                 That&#39;s nearly $5,000 more—simply by following smarter
+//                 signals.
+//               </p>
+//               <p className="head-para">
+//                 Lesson: Catch the right trends, and you ride higher.
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+
+//       {/* <!-- End Of Performance Section --> */}
+
+//       <Community />
+//       <JoinWaitList />
+//     </>
+//   );
+// }
